3088fcdc9a43822254b35666c8920da11ad15e55
gba-exe <125586278+gba-exe@users.noreply.github.com>
2025-02-20 20:10:05 -0300

    feat(): starting to deal with dataframes

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import pandas as pd
 
 app = Flask (__name__)
 CORS(app)
 
-df_transactions = pd.read_csv("transactions.csv", delimiter=";")
+df_transactions = pd.DataFrame(columns=['name', 'description', 'functional', 'keyUser'])
 
 @app.route("/transactions", methods=["GET"])
-def transactions():
-    print(df_transactions)
-    return jsonify(df_transactions.to_dict())
+def transactions_get():
+    return jsonify(df_transactions.to_dict(orient="records"))
 
 @app.route("/transactions/code", methods=["GET"])
 def transactions_code():
-    print(df_transactions)
     return jsonify(df_transactions["Transaction Code"].to_list())
+
+@app.route("/transactions", methods=["POST"])
+def transactions_post():
+    global df_transactions
+    transactions = request.get_json()
+
+    if not transactions or not isinstance(transactions, list):
+        print("a")
+        return jsonify({"error": "Invalid input. A list of transactions was expected."}), 400
+
+    required_keys = {'name', 'description', 'functional', 'keyUser'}
+
+    for transaction in transactions:
+        if not required_keys.issubset(transaction):
+            print("b")
+            return jsonify({"error": "Each transaction must have a name, description, functional and a keyUser."}), 400
+
+    new_transactions = pd.DataFrame(transactions)
+    df_transactions = pd.concat([df_transactions, new_transactions], ignore_index=True)
+
+    return jsonify({"message": "Transactions recieved successfully", "transactions": transactions}), 200
 
 
 if __name__ == "__main__":
