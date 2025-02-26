2db2dfeab22d5b0bbcb567640ab1c54608f2690c
gba-exe <125586278+gba-exe@users.noreply.github.com>
2025-02-25 21:28:46 -0300

    fix(): endpoints corrected

--- main.py
+++ main.py
@@ -2,39 +2,57 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 import pandas as pd
 
-app = Flask (__name__)
+app = Flask(__name__)
 CORS(app)
 
-df_transactions = pd.DataFrame(columns=['name', 'description', 'functional', 'keyUser'])
+df_transactions = pd.DataFrame(
+    columns=['transaction', 'description', 'functional', 'keyUser'])
+
+df_roles = pd.DataFrame(
+    columns=['role', 'keyUser'])
+
+df_csv = pd.read_csv("transactions.csv")
+df_transactions = pd.concat([df_transactions, df_csv], ignore_index=True)
+
 
 @app.route("/transactions", methods=["GET"])
 def transactions_get():
     return jsonify(df_transactions.to_dict(orient="records"))
 
+
 @app.route("/transactions/code", methods=["GET"])
 def transactions_code():
-    return jsonify(df_transactions["Transaction Code"].to_list())
+    return jsonify(df_transactions["transaction"].to_list())
+
 
 @app.route("/transactions", methods=["POST"])
 def transactions_post():
     global df_transactions
     transactions = request.get_json()
 
+    print(transactions)
     if not transactions or not isinstance(transactions, list):
-        print("a")
-        return jsonify({"error": "Invalid input. A list of transactions was expected."}), 400
+        return jsonify({
+            "error": "Invalid input. A list of transactions was expected."
+        }), 400
 
-    required_keys = {'name', 'description', 'functional', 'keyUser'}
+    required_keys = {'transaction', 'description', 'functional', 'keyUser'}
 
     for transaction in transactions:
         if not required_keys.issubset(transaction):
-            print("b")
-            return jsonify({"error": "Each transaction must have a name, description, functional and a keyUser."}), 400
+            return jsonify({
+                "error": "Each transaction must have a transaction, " +
+                "description, functional and a keyUser."
+            }), 400
 
     new_transactions = pd.DataFrame(transactions)
-    df_transactions = pd.concat([df_transactions, new_transactions], ignore_index=True)
+    df_transactions = pd.concat(
+        [df_transactions, new_transactions], ignore_index=True)
 
-    return jsonify({"message": "Transactions recieved successfully", "transactions": transactions}), 200
+    return jsonify({
+        "message": "Transactions recieved successfully",
+        "transactions": transactions
+    }), 200
 
 
 if __name__ == "__main__":
